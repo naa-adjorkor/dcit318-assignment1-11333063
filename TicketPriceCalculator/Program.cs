1f33003f1a5ffd8285033ae3b7c52974d88ea554
naakrong-adjei <naaadjorkorakrongadjei@gmail.com>
2025-08-04 23:11:55 +0000

    Added Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int price = (age <= 12 || age >= 65) ? 7 : 10;
+
+            Console.WriteLine($"Ticket price: GHC {price}");
+        }
+    }
+}
