92f4e373c0be185e9e2a47c61f72d93d8e376dfd
naakrong-adjei <naaadjorkorakrongadjei@gmail.com>
2025-08-04 23:21:55 +0000

    Added Triangle Type Identifier

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter side 3: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b && b == c)
+                Console.WriteLine("Equilateral");
+            else if (a == b || b == c || a == c)
+            Console.WriteLine("Isosceles");
+            else
+                Console.WriteLine("Scalene");
+        }
+    }
+}
